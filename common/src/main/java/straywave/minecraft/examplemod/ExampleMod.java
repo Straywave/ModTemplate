e9f26e74982ea29193ac472c83127c08a28f624b
Straywave <121810805+Straywave@users.noreply.github.com>
2023-10-06 14:17:15 +0400

    1.20.1; Drop 1.16.5; Gradle 8.4; Bump deps

--- common/src/main/java/straywave/minecraft/examplemod/ExampleMod.java
+++ common/src/main/java/straywave/minecraft/examplemod/ExampleMod.java
@@ -7,12 +7,12 @@ public class ExampleMod {
     public static Logger LOGGER = LogManager.getLogger("examplemod");
 
     public static void init() {
-        #if MC_1_16_5
-        LOGGER.info("This is Minecraft version 1.16.5!");
-        #elif MC_1_18_2
+        #if MC_1_18_2
         LOGGER.info("This is Minecraft version 1.18.2!");
         #elif MC_1_19_2
         LOGGER.info("This is Minecraft version 1.19.2!");
+        #elif MC_1_20_1
+        LOGGER.info("This is Minecraft version 1.20.1!");
         #endif
     }
 }
